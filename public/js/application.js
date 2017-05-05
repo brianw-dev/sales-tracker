7c3120cc4b01347afa751bb6e9c7f5a828244395
Brian Wong <email": null>
2017-05-05 14:54:59 -0700

    add materialize css and young thug

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
+  $(".nav-wrapper").on("click", ".login", function(event){
+    event.preventDefault()
+    var $login = $(this)
+    var url = $login.attr('href')
 
+    $.ajax({
+      url: url,
+      method: "get"
+    })
+    .done(function(response) {
+      $(".form").empty()
+      $(".form").append(response)
+    })
+  })
+
+    $(".nav-wrapper").on("click", ".new-account", function(event){
+    event.preventDefault()
+    var $newaccount = $(this)
+    var url = $newaccount.attr('href')
+
+    $.ajax({
+      url: url,
+      method: "get"
+    })
+    .done(function(response) {
+      $(".form").empty()
+      $(".form").append(response)
+    })
+  })
 });
